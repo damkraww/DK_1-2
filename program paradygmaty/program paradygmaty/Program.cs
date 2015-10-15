36f50a8de5c3748bfdf81941f0ddb5810164b347
damkraww <damkraww@gmail.com>
2015-10-16 00:36:58 +0200

    Lab1-2

---
+++ program paradygmaty/program paradygmaty/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_paradygmaty
+{
+    using System;
+ 
+abstract class Zoo
+{
+    abstract public void Breath();
+}
+ 
+class Demon : Zoo
+{
+    public override void Breath()
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Pierwsze labki z programowania obiektowego");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("dowiedzialem sie ze c# lepszy od c++");
+    }
+}
+ 
+static class boo
+{
+    static public int asd;
+    static public int qwe
+    {
+        get
+        {
+            return asd ;
+        }
+        set
+        {
+            asd = value;
+        }
+    }
+}
+ 
+class Program
+{
+ 
+    static void Main(string[] args)
+    {
+ 
+        boo.qwe = 5;
+        Console.WriteLine(boo.asd + boo.qwe);
+ 
+        Zoo MyZoo = new Demon();
+        MyZoo.Breath();
+        Console.ReadLine();
+    }
+}
+    
+}
